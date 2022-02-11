dcd36ef66e8d92b7edcf7c1bd5c27cef757235d3
imdad Adelabou <imdadadelabou0@gmail.com>
2022-02-11 03:23:54 +0100

    get token price with Kucoin Api

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from getTokenPrice import GetTokenPrice;
 
 
 def main():
-    # price = GetTokenPrice("COINGECKO", "BTC")
-    # price.price_by_coinbase()
-    # print("====>>BINANCE<<<======")
-    # binance_api = BinanceApi()
-    # print(str(binance_api.get_token_price()))
-    print("====>> KUCOIN <<=====")
-    result = KucoinApi().get_token_price()
-    print(result)
+    try:
+        # price = GetTokenPrice("COINGECKO", "BTC")
+        # price.price_by_coinbase()
+        # print("====>>BINANCE<<<======")
+        # binance_api = BinanceApi()
+        # print(str(binance_api.get_token_price()))
+        print("====>> KUCOIN <<=====")
+        result = KucoinApi().get_token_price()
+        print(result)
+    except Exception as e:
+        print(e)
 
 main()
